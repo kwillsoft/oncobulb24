cc4ddcb00018da41440b0091d58995c0762d20b6
kwillsoft <kwillsoftware@gmail.com>
2021-08-24 18:18:37 -0700

    done

--- resources/views/bladder.blade.php
+++ resources/views/bladder.blade.php
@@ -20,7 +20,7 @@ function getData() {
 getinfo = async () => {
     var API_KEY = '44bad42c863c717dd4e6ae71efb182ab1009';
     const api_call = await fetch(
-        `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=20&term=bladder+cancer+herb+plant&api_key=${API_KEY}&usehistory=y`
+        `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=20&term=bladder+cancer+herb+plant&api_key=44bad42c863c717dd4e6ae71efb182ab1009&usehistory=y`
     );
     const data = await api_call.text();
     if (data) {
