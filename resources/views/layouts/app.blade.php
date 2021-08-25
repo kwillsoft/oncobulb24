fbb419f27ac93796f9f7e1d892eca57f398b88f1
kwillsoft <kwillsoftware@gmail.com>
2021-08-24 19:21:31 -0700

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ oncoBULB') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     @yield('scripts')
