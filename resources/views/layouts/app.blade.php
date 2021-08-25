3ddd8f601912e27b545b584bd7afb24d7eed789f
kwillsoft <kwillsoftware@gmail.com>
2021-08-24 20:08:33 -0700

    done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>oncoBULB | Plant-Based Cancer Research At Your Fingertips</title>
 
     <!-- Scripts -->
     @yield('scripts')
