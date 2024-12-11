4040cd55e2afc9d26f633a008283badc74f6cadd
Ken <kwillsoftware@gmail.com>
2024-12-11 15:00:38 -0800

    wednesday

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,10 +12,7 @@
     <title>oncoBULB | Plant-Based Cancer Research At Your Fingertips</title>
 
     <!-- Scripts -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
+    @yield('scripts')
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -25,14 +22,14 @@
 
     <!-- Styles -->
     
-    <link href="{{ asset('css/app.css') }}"  rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
 </head>
 
-<body>
-    <div id="app"  class="app_body">
+<body class="app_body">
+    <div id="app">
      
 
-        <main class="py-4"  class="app_body">
+        <main class="py-4">
             @yield('content')
         </main>
     </div>
