56c7891e7828630051c9f526d7fa3ad328ccbf84
Ken <kwillsoftware@gmail.com>
2024-12-11 16:58:20 -0800

    wednesday

--- resources/views/kidney.blade.php
+++ resources/views/kidney.blade.php
@@ -6,72 +6,44 @@
 @section('scripts')
 
 <script>
-getinfo = async () => {
-    var API_KEY = '{{ env('API_KEY') }}';
-    
-    try {
-        // First API call: Get WebEnv and QueryKey
-        const api_call = await fetch(
-            `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=20&term=plant+cancer+kidney+herb+NOT+id=33634751&api_key=${API_KEY}&usehistory=y`
-        );
-        const data = await api_call.text();
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(data, "text/xml");
-        const Web_Env = xmlDoc.getElementsByTagName("WebEnv")[0].childNodes[0].nodeValue;
-        const Query_Key = xmlDoc.getElementsByTagName("QueryKey")[0].childNodes[0].nodeValue;
-
-        // Second API call: Fetch abstracts
-        const api_callb = await fetch(
-            `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmax=20&api_key=${API_KEY}&retmode=xml&rettype=abstract&query_key=${Query_Key}&WebEnv=${Web_Env}`
-        );
-        const datab = await api_callb.text();
-        
-        // Parse the XML response from the second API call
-        const xmlDocb = parser.parseFromString(datab, "text/xml");
-        const articles = xmlDocb.getElementsByTagName("PubmedArticle");
-        
-        let formattedArticles = '';
-        for (let i = 0; i < articles.length; i++) {
-            const article = articles[i];
-            const title = article.getElementsByTagName("ArticleTitle")[0]?.textContent || "No Title Available";
-            const abstract = article.getElementsByTagName("AbstractText")[0]?.textContent || "No Abstract Available";
-            const pubDate = article.getElementsByTagName("PubDate")[0]?.textContent || "Unknown Date";
-            
-            // Limit authors to only the first one
-            const authors = article.getElementsByTagName("Author");
-            let authorNames = '';
-            if (authors.length > 0) {
-                authorNames = authors[0].getElementsByTagName("LastName")[0]?.textContent + ", " + authors[0].getElementsByTagName("ForeName")[0]?.textContent;
-            }
-
-            formattedArticles += `<div class="article-item">
-                <h3 class="article-title">${title}</h3>
-                <p class="article-date"><strong>Publication Date:</strong> ${pubDate}</p>
-                <p class="article-authors"><strong>First Author:</strong> ${authorNames}</p>
-                <p class="article-abstract">${abstract}</p>
-            </div><hr>`;
-        }
-
-        // Display articles
-        const today = new Date().toDateString();
-        const header = `<strong>Peer-Reviewed Data From the National Center For Biotechnology Information (NCBI)</strong><br>${today}<br>`;
-        document.getElementById("thee_data").innerHTML = header + formattedArticles;
-
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        document.getElementById("thee_data").innerHTML = `<p>There was an error fetching the data. Please try again later.</p>`;
-    }
-};
-
-// Ensure the function runs when the button is clicked
-getData = () => {
+function getData() {
     document.getElementById("top_box").style.display = "none";
     document.getElementById("c_button").style.display = "none";
     document.getElementById("backbutton1").style.display = "none";
     document.getElementById("back").style.display = "block";
     document.getElementById("thee_data").style.display = "block";
-    getinfo();
-};
+
+}
+</script>
+<script>
+getinfo = async () => {
+    var API_KEY = '{{ env('API_KEY') }}';
+    const api_call = await fetch(
+        `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=20&term=plant+cancer+kidney+herb+NOT+id=33634751&api_key=${API_KEY}&usehistory=y`
+    );
+    const data = await api_call.text();
+    if (data) {
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(data, "text/xml");
+        const Web_Env = xmlDoc.getElementsByTagName("WebEnv")[0].childNodes[0].nodeValue;
+        const Query_Key = xmlDoc.getElementsByTagName("QueryKey")[0].childNodes[0].nodeValue;
+        const api_callb = await fetch(
+            `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmax=20&api_key=${API_KEY}&retstart=2&retmode=json&rettype=abstract&query_key=${Query_Key}&WebEnv=${Web_Env}`
+        );
+        const datab = await api_callb.text();
+           
+        var Today = () => {
+        var d = new Date();
+        return d.toDateString();
+         }
+
+         const theetitle = "Peer-Reviewed Data From the National Center For Biotechnology Information(NCBI)<br> "+ Today() +"<br>"
+        var boldtitle =  theetitle.bold();
+        var boldtitle= boldtitle
+        document.getElementById("thee_data").innerHTML = (boldtitle) + (datab);
+    }
+}
+getinfo();
 </script>
 
 @endsection
