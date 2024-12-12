62b31a72630e8f55e4b1f3c432a429188d8ee4f5
Ken <kwillsoftware@gmail.com>
2024-12-11 20:59:48 -0800

    wednesday

--- resources/views/kidney.blade.php
+++ resources/views/kidney.blade.php
@@ -7,30 +7,46 @@
 
 <script>
 getinfo = async () => {
+    var API_KEY = '{{ env('API_KEY') }}';
+    
     try {
-        // First API call: Get WebEnv and QueryKey via proxy
-        const api_call = await fetch(`/api/proxy/pubmed?term=plant+cancer+kidney+herb+NOT+id=33634751`);
+        // First API call: Get WebEnv and QueryKey
+        const api_call = await fetch(
+            `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=20&term=plant+cancer+kidney+herb+NOT+id=33634751&api_key=${API_KEY}&usehistory=y`
+        );
         const data = await api_call.text();
+        console.log(data); // Debug raw XML response
+
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(data, "text/xml");
-        const Web_Env = xmlDoc.getElementsByTagName("WebEnv")[0].childNodes[0].nodeValue;
-        const Query_Key = xmlDoc.getElementsByTagName("QueryKey")[0].childNodes[0].nodeValue;
 
-        // Second API call: Fetch abstracts via proxy
-        const api_callb = await fetch(`/api/proxy/efetch?query_key=${Query_Key}&web_env=${Web_Env}`);
+        const Web_Env_Element = xmlDoc.getElementsByTagName("WebEnv")[0];
+        const Query_Key_Element = xmlDoc.getElementsByTagName("QueryKey")[0];
+
+        if (!Web_Env_Element || !Query_Key_Element) {
+            throw new Error("Missing WebEnv or QueryKey in the API response");
+        }
+
+        const Web_Env = Web_Env_Element.childNodes[0]?.nodeValue;
+        const Query_Key = Query_Key_Element.childNodes[0]?.nodeValue;
+
+        // Second API call: Fetch abstracts
+        const api_callb = await fetch(
+            `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmax=20&api_key=${API_KEY}&retmode=xml&rettype=abstract&query_key=${Query_Key}&WebEnv=${Web_Env}`
+        );
         const datab = await api_callb.text();
 
         // Parse the XML response from the second API call
         const xmlDocb = parser.parseFromString(datab, "text/xml");
         const articles = xmlDocb.getElementsByTagName("PubmedArticle");
-        
+
         let formattedArticles = '';
         for (let i = 0; i < articles.length; i++) {
             const article = articles[i];
             const title = article.getElementsByTagName("ArticleTitle")[0]?.textContent || "No Title Available";
             const abstract = article.getElementsByTagName("AbstractText")[0]?.textContent || "No Abstract Available";
             const pubDate = article.getElementsByTagName("PubDate")[0]?.textContent || "Unknown Date";
-
+            
             // Limit authors to only the first one
             const authors = article.getElementsByTagName("Author");
             let authorNames = '';
