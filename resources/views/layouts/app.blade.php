89f57eb54a3e32a9a5fc2b3c7a694132a76e3b8b
Ken <kwillsoftware@gmail.com>
2024-12-11 21:45:51 -0800

    wednesday

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,36 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400&family=Nunito&display=swap" rel="stylesheet">
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>oncoBULB | Plant-Based Cancer Research At Your Fingertips</title>
 
     <!-- Scripts -->
+    @vite(['resources/js/app.js'])
     @yield('scripts')
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;400&display=swap" rel="stylesheet">
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 
-<body class="app_body">
-    <div id="app">
-     
+<body class="antialiased">
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    <!-- Your header and navigation links -->
+    <nav>
+        <!-- Your links for navigation -->
+    </nav>
+
+    <!-- Main content -->
+    @yield('content')
+
 </body>
 
 </html>
